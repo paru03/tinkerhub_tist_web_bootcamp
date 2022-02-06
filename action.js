3ea1be58490019e84470fc77e67db1725bea612d
paru03 <99135434+paru03@users.noreply.github.com>
2022-02-06 22:07:23 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,15 @@
+function changeToYellow() {
+  document.body.style.backgroundColor = "yellow";
+}
+
+function changeToViolet() {
+  document.body.style.backgroundColor = "purple";
+}
+
+function changeToBlack() {
+  document.body.style.backgroundColor = "black";
+}
+
+function changeToBlue() {
+  document.body.style.backgroundColor = "purple";
+}
